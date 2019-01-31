70204f4a2452cce990f6f5716266d19895cadac2
Brendon Waters <brendon.waters117@gmail.com>
2019-01-30 17:16:26 -0800

    Developed NewMsg.jsx component in MsgBoard.jsx

--- app_server/components/MsgBoard.jsx
+++ app_server/components/MsgBoard.jsx
@@ -1,16 +1,25 @@
 const React = require("react");
 const MsgList = require("./MsgList.jsx");
+const NewMsg = require("./NewMsg.jsx");
 
 class MsgBoard extends React.Component {
     constructor(props) {
       super(props);
       this.state = { messages: this.props.messages};
+      this.addMessage = this.addMessage.bind(this);
+    }
+
+    addMessage() {
+        // TO DO MAKE API CALL TO STORE A NEW MESSAGE IN UPDATE VAR MESSAGE
     }
   
     render() {
         return (
-            <MsgList messages={ this.state.messages }/>
-        )
+            <div>
+                <NewMsg addMsgCallback = { this.addMessage} />
+                <MsgList messages={ this.state.messages }/>
+            </div>
+        );
     }
 }
 
