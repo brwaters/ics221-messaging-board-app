4fd687495412df06e244ea912bfdb0792aea988b
Brendon Waters <brendon.waters117@gmail.com>
2019-03-21 15:01:36 -0700

    Implemented auth into posting msg api call

--- api_server/routes/api_router.js
+++ api_server/routes/api_router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const msgAPIController = require('../controllers/msg-api');
 const userAPIController = require('../controllers/user-api');
 
 router.route('/msgs')
 .get(msgAPIController.getAllMessagesOrderedByLastPosted)
-.post(msgAPIController.addNewMessage);
+.post(passport.authenticate('basic', { session: false }), msgAPIController.addNewMessage);
 
 
 router.post('/users', userAPIController.registerNewUser);
