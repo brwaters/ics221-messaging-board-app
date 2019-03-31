539044d35065e223db0a746bb3bab97a38b40eba
Brendon Waters <brendon.waters117@gmail.com>
2019-03-31 13:26:21 -0700

    Trying with db deploy

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//require('./api_server/db');
+require('./api_server/db');
 
 var appRouter = require('./app_server/routes/app_router.js');
-//var apiRouter = require('./api_server/routes/api_router.js');
+var apiRouter = require('./api_server/routes/api_router.js');
 var app = express();
 
 // view engine setup
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 // routes
 app.use('/', appRouter);
-//app.use('/api/v1', apiRouter);
+app.use('/api/v1', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
